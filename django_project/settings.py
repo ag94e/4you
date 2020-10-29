a3ae350c2fdf8b5ee65e4dfe6b1b59039ea6b7f5
Antonio Guerrero <a.guerrero.esp94@gmail.com>
2020-10-29 13:14:01 -0600

    fixed errors

--- django_project/settings.py
+++ django_project/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '=b2!5z6dh=*6s-^^&hy^wd&@)tnmxk-7b(5bf$e4rddm*bgda6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
